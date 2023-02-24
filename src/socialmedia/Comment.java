40afde8bdff72213277b185dc1592f6f4ebb2815
Jonathan <jmrutland@gmail.com>
2023-02-24 16:45:50 +0000

    Created comment constructor

--- src/socialmedia/Comment.java
+++ src/socialmedia/Comment.java
@@ -4,7 +4,8 @@ public class Comment extends Post{
     private Post originalPost;
 
     public Comment(Account poster, Post originalpost, String message){
-        
+        super(poster, message);
+        this.originalPost = originalpost;
     }
 
 
